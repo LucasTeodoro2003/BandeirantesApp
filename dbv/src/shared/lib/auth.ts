1d4ae09d9382d540863dc63b5759e5fc45268287
Lucas <lucasteodoro.lm@gmail.com>
2026-02-05 17:36:56 -0300

    feat: init OTP

--- dbv/src/shared/lib/auth.ts
+++ dbv/src/shared/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth, string } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import { prisma } from './prisma'
+import { emailOTP } from 'better-auth/plugins'
 
 const isDevelopment = process.env.NODE_ENV === "development"
 
@@ -15,8 +16,8 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  user:{
-    additionalFields:{
+  user: {
+    additionalFields: {
       clubId: {
         type: "string"
       },
@@ -29,4 +30,17 @@ export const auth = betterAuth({
       "https://*.app.github.dev", // Aceita qualquer URL do Codespaces
     ]
     : [process.env.BETTER_AUTH_URL || "https://seu-dominio.com"],
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp, type }) {
+        if (type === "sign-in") {
+          // Send the OTP for sign in
+        } else if (type === "email-verification") {
+          // Send the OTP for email verification
+        } else {
+          // Send the OTP for password reset
+        }
+      },
+    })
+  ]
 })
